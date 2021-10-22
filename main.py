d743b6d92e56e619d00092a5a242d9b380df787f
Ubuntu <ubuntu@ip-172-31-85-153.ec2.internal>
2021-10-22 00:09:02 +0000

    finished

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Created by: Daniel Pawelko
+# Created on: Oct 2021
+# This is a guessing game
+
+from random import randint
+
+def main():
+    # This function inputs guess and outputs if correct or not
+
+    # store const
+    NUM = randint(0, 9)
+
+    # input
+    guess = int(input("Enter guess(0-9): "))
+
+    # process/output
+    if guess == NUM:
+        print("You guessed correctly!")
+    else:
+        print(f"Incorrect, the number was {NUM}.")
+
+    # output finished
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
